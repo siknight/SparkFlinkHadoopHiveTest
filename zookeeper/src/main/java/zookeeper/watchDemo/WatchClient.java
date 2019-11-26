95549cd3be34b06d62b4cd6ee8741f260a10b476
lisi <1786678583@qq.com>
2019-11-26 17:33:16 +0800

    spark,flink,mapreduce,zookeeper

--- zookeeper/src/main/java/zookeeper/watchDemo/WatchClient.java
+++ zookeeper/src/main/java/zookeeper/watchDemo/WatchClient.java
@@ -4,7 +4,6 @@ import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
-import org.junit.Test;
 
 import java.io.IOException;
 import java.util.List;
