0b6b5ff85c4c5e42c0cc64f5c724923b461c2a96
lisi <1786678583@qq.com>
2019-11-27 20:52:36 +0800

    spark,flink,mapreduce,zookeeper

---
+++ Scala/src/main/scala/scalatest/first/mainMethod/MainTest.scala
@@ -0,0 +1,15 @@
+package scalatest.first.mainMethod
+
+/**
+  * scala第一个程序，用于测试scala main方法
+  *
+  * main方法的类必须是object修饰
+  */
+object MainTest {
+
+  def main(args: Array[String]) {
+    //后面的;可以省略
+    println("first scala main method");
+  }
+
+}
